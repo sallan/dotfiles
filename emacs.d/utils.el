668c83eb613e7a34a9360082fd7f0d6bc426f335
Steve Allan <sallan@tableau.com>
2017-01-31 11:29:41 -0800

    Mike's swap window function and eshell-here.

--- emacs.d/utils.el
+++ emacs.d/utils.el
@@ -136,3 +136,44 @@
   (dired-do-shell-command (format "tar czf %s *" tarfile)
                           nil
                           (dired-get-marked-files t)))
+
+;;; Function from Mike to swap windows
+(defun swap-windows ()
+  (interactive)
+  (if (not (= 2 (count-windows)))
+      (error "Frame must have exactly two windows for swap-windows"))
+  (let  ((buf1 (current-buffer))
+         (buf2 (progn
+                 (other-window 1)
+                 (current-buffer))))
+    (switch-to-buffer buf1)
+    (other-window 1)
+    (switch-to-buffer buf2)))
+
+
+;;; eshell here and kill function from:
+;;; http://www.howardism.org/Technical/Emacs/eshell-fun.html
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
