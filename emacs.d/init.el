12283dec959a2e16787eb09f114d9ae552978573
Steve Allan <sallan@tableau.com>
2017-03-10 09:24:15 -0800

    Add melpa path

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
 ;;;; The order here is important
@@ -6,14 +13,21 @@
 (load-file "~/.emacs.d/utils.el")
 (load-file "~/.emacs.d/global-keybindings.el")
 (load-file "~/.emacs.d/modes/cperl.el")
-;;(load-file "~/.emacs.d/modes/iswitch.el")
+(load-file "~/.emacs.d/modes/iswitch.el")
 
 (put 'dired-find-alternate-file 'disabled nil)
 (global-auto-revert-mode 1)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
-;; (server-start)
-;; (eshell)
+(server-start)
 
 (setenv "P4EDITOR" "emacsclient")
+
+;; Start in the work directory
+(dired "~/Work")
+;;(eshell)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
